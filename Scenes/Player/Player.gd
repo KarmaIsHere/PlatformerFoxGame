a65eb1090561266425dc891f0d083eff598f088f
KarmaIsHere <letrixswitch@gmail.com>
2025-12-12 13:34:14 +0200

    Project setup

---
+++ Scenes/Player/Player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+class_name Player
+
+const GRAVITY: float = 690.0
+
+func _ready() -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
+	velocity.y += GRAVITY * delta
+	move_and_slide()
